007f0a73c0c478131244c6ba14c539b55ad294f0
jaydewong <jayde_wong@yahoo.com>
2023-04-21 17:28:18 +0900

    set up typescript compilation environment, testing getting input from terminal

--- main-jayde.ts
+++ main-jayde.ts
@@ -1,1 +1,18 @@
-// hello
+//creating a basic project - testing out how to write and compile typescript 
+
+//for this section to work you have to run with "node main-jayde.ts"
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+readline.question(`Give me an idiom?`, idiom => {
+  console.log(`Hi ${idiom}!`);
+  readline.close();
+});
+
+/*export function idiom_detect(who: string | null = idiom): string {
+
+    return "This is an idiom";
+}*/
+
