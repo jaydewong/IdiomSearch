89693ea042311be25a39ead09b278789a412bf21
jaydewong <jayde_wong@yahoo.com>
2023-04-21 19:39:52 +0900

    used JSPyBridge to call python function from a js file successfully

--- main-jayde.js
+++ main-jayde.js
@@ -1,9 +1,14 @@
-"use strict";
+//"use strict";
 //creating a basic project - testing out how to write and compile typescript 
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.idiom_detect = void 0;
-const idiom = 'the world is my oyster';
-function idiom_detect(who = idiom) {
-    return "This is an idiom";
-}
-exports.idiom_detect = idiom_detect;
+// Object.defineProperty(exports, "__esModule", { value: true });
+// exports.idiom_detect = void 0;
+// const idiom = 'the world is my oyster';
+// function idiom_detect(who = idiom) {
+//     return "This is an idiom";
+// }
+// exports.idiom_detect = idiom_detect;
+
+import { python } from "pythonia"; 
+const searchResult = await python("./main-elizabeth.py"); 
+console.log(await searchResult.main()); 
+python.exit(); 
--- main-jayde.ts
+++ main-jayde.ts
@@ -1,6 +1,8 @@
 //creating a basic project - testing out how to write and compile typescript 
 
-//for this section to work you have to run with "node main-jayde.ts"
+//testing integrating with python code in elizabeth's file 
+
+/*for this section to work you have to run with "node main-jayde.ts"
 const readline = require('readline').createInterface({
     input: process.stdin,
     output: process.stdout,
@@ -9,7 +11,7 @@ const readline = require('readline').createInterface({
 readline.question(`Give me an idiom?`, idiom => {
   console.log(`Hi ${idiom}!`);
   readline.close();
-});
+});*/
 
 /*export function idiom_detect(who: string | null = idiom): string {
 
