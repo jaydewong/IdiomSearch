b76e7f59cd0cae10ec00c63404445a4e7e837679
Elizabeth Yeh <elizabethyeh@Elizabeths-MacBook-Pro-2.local>
2023-04-25 13:27:53 +0900

    minor changes to python file; renamed main files

--- main.py
+++ main.py
@@ -12,16 +12,28 @@ import sys
 import spacy
 from idiomatch import Idiomatcher
 
-def main():
+run_setup = False
+
+def setup():
     nlp = spacy.load("en_core_web_sm")  # idiom matcher needs an nlp pipeline; Currently supports en_core_web_sm only.
     idiomatcher = Idiomatcher.from_pretrained(nlp)  # this will take approx 50 seconds.
+    run_setup = True
 
-    while (True):
-        test = input("test input: ")
+def main():
+    if (not run_setup) :
+        setup()
+
+    # this always times out on us
+    test = input("test input (enter 'stop' to exit): ")
+
+    while (test != "stop"):
         print(test)
         doc = nlp(test)  # process the sentence with an nlp pipeline
         print(idiomatcher.identify(doc))  # identify the idiom in the sentence
-        test = input("test input: ")
+        test = input("test input (enter 'stop' to exit): ")
+
+    print("Program stopped. "); 
 
 if __name__ == '__main__':
+    setup()
     main()
