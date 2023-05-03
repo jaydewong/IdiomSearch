25490c524a4b92292d3054365178562ab004078c
jaydewong <jayde_wong@yahoo.com>
2023-05-03 23:13:34 +0900

    working to parse results from idiomatcher

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ import { python } from "pythonia";
 //access python idiomatch file 
 const searchResult = await python("./main.py");
 
-const userInput = ["this guys is balls out insane", "there will be blood on your hands", "on the fence"]; 
+const userInput = ["this guys is balls out insane"]; 
 
 for(let i = 0; i < userInput.length; i++){
     if(i === 0){
@@ -20,7 +20,35 @@ for(let i = 0; i < userInput.length; i++){
         console.log(await searchResult.setup()); 
         console.log("setup complete"); 
     }
-    console.log(await searchResult.is_idiom(userInput[i]));
+
+    const result = async () => {
+        const isIdiom = await searchResult.is_idiom(userInput[i]); 
+      
+        return isIdiom; 
+    }
+
+    let readResult = await result(); 
+    readResult.replaceAll('\'', '"')
+    //[{'idiom': 'balls-out', 'span': 'balls out', 'meta': (2876800142358111704, 3, 5)}]
+
+    let resultJSON = JSON.parse(JSON.stringify(readResult));  //turn it into JSON format
+    console.log(resultJSON.idiom); 
+
+
+    //readResult = readResult.replace("\"'idiom'\":", "\"idiom\":");
+    // const parsedResult = JSON.parse(readResult);
+    // console.log(parsedResult.idiom); 
+    //console.log();
+
+
+    //if the input is an idiom
+    // if(result != null){
+    //     let idiom = await result(); 
+    //     console.log(idiom[0]); 
+    // }else{
+    //     console.log("not an idiom");
+    // }
 }
+
 
 python.exit(); 
